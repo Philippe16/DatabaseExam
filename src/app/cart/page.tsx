d3dc1fec4c7b72d2c13e7dbb5a33ce9e8c65fde8
Philippe16 <philippe.suenson@hotmail.com>
2024-05-26 15:40:45 +0200

    bønne let me love you doooooown

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import { CartContext } from "../context/cartContext";
 
 const Cart = () => {
@@ -11,26 +12,31 @@ const Cart = () => {
     return total + (isNaN(price) ? 0 : price);
   }, 0);
 
+
+  const handleBuyNow = () => {
+    // Handle the buy now action
+    alert('Proceeding to checkout...');
+  };
+
   return (
-    <div className="text-white">
-      <p className="text-lg font-bold">
-        Hej Phil denne kode er bare til at vise dig hvordan du kan bruge context
-        til cart siden. Du skal gå ind og tilføje skins for at se dem her. Fjern
-        denne kode og lav din egen.
-      </p>
+    <div className="text-white container mx-auto py-10">
       <div className="mt-10 flex flex-col">
-        <h1 className="text-2xl">Skins i kurv:</h1>
+        <h1 className="text-2xl mb-6">Skins i kurv:</h1>
         <div className="px-2 flex flex-col gap-5">
           {items.map((item) => (
             <div
-              className="flex flex-row items-center gap-5 mt-3"
+              className="flex flex-row items-center gap-5 mt-3 bg-[#33383a] p-4 rounded-lg"
               key={item.id}
             >
-              <div className="flex flex-col">
-                <span>name: {item.name}</span>
-                <span>price: ${item.price}</span>
+              <div className="flex-shrink-0">
+                <div className="relative h-40 w-40">
+                  <Image src={item.src} layout="fill" objectFit="contain" alt={`image of ${item.name}`} />
+                </div>
               </div>
-              -
+              <div className="flex flex-col flex-grow">
+                <span className="text-xl font-bold">{item.name}</span>
+                <span className="text-lg">${item.price}</span>
+              </div>
               <button
                 className="bg-red-500 w-20 h-10"
                 onClick={() =>
@@ -42,13 +48,18 @@ const Cart = () => {
             </div>
           ))}
           <div className="mt-5">
-            <span className="font-bold">Total Cart Price:</span> $
-            {totalCartPrice}
+            <span className="font-bold">Total Cart Price:</span> ${totalCartPrice.toFixed(2)}
           </div>
         </div>
+      </div>
+      <div className="w-full bg-[#48a9cb] hover:bg-[#367e98] h-10 rounded-lg flex justify-center items-center mt-6">
+        <button className="w-full h-full text-white" onClick={handleBuyNow}>
+          Place order
+        </button>
       </div>
     </div>
   );
 };
 
 export default Cart;
+
