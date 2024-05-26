e96f75212a2a9dc497e18c35e45071555424fd4a
Mikkel Bentsen <bentsen405@gmail.com>
2024-05-26 15:46:03 +0200

    sjdkf

---
+++ src/lib/mongodb/models/Order.ts
@@ -0,0 +1,24 @@
+import { Skin } from "@/types/skins.t";
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+  items: Skin[];
+  total: number;
+}
+
+const OrderSchema = new Schema({
+  items: [
+    {
+      id: Number,
+      name: String,
+      src: String,
+      price: String,
+      weapon_id: Number,
+    },
+  ],
+  total: Number,
+});
+
+const Order = mongoose.model<IOrder>("Order", OrderSchema);
+
+export default Order;
--- src/lib/mongodb/queries.ts
+++
@@ -1,7 +0,0 @@
-import connectMongo from "./config";
-import User from "./models/User"; // Assume you have a User model defined
-
-export const getUsers = async () => {
-  await connectMongo();
-  return User.find({});
-};
