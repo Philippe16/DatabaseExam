c03b02a17267432f0f6065a949982b0f75037969
Mikkel Bentsen <bentsen405@gmail.com>
2024-05-26 20:06:38 +0200

    phil code refactored

---
+++ src/app/api/(mongodb)/orders/[id]/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import Order, { IOrder } from "@/lib/mongodb/models/Order";
+import connectMongo from "@/lib/mongodb/config";
+
+// If ID is provided, fetch a specific order and if no ID is provided, fetch all orders
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<NextResponse> {
+  await connectMongo();
+  try {
+    const orderId = params.id;
+    const order: IOrder | null = await Order.findById(orderId);
+
+    if (!order) {
+      return new NextResponse(JSON.stringify({ message: "Order not found" }), {
+        status: 404,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    // Return the order if found
+    return new NextResponse(JSON.stringify(order), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    return new NextResponse(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
---
+++ src/app/api/(mongodb)/orders/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from "next/server";
+import Order, { IOrder } from "@/lib/mongodb/models/Order";
+import connectMongo from "@/lib/mongodb/config";
+import { Skin } from "@/types/skins.t";
+
+export async function GET(): Promise<NextResponse> {
+  await connectMongo();
+  try {
+    const orders: IOrder[] = await Order.find();
+
+    // Return the list of orders
+    return new NextResponse(JSON.stringify(orders), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    return new NextResponse(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  await connectMongo();
+
+  try {
+    const { items, total }: { items: Skin[]; total: number } = await req.json();
+
+    const newOrder: IOrder = new Order({
+      items,
+      total,
+    });
+    await newOrder.save();
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "Order created successfully",
+        order: newOrder,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : String(error);
+
+    // Return error response with status code 500
+    return new NextResponse(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
